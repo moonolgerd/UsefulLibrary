c3bd963c176f41bda2a03df010f70f60985f71e8
Oleg <moonolgerd@gmail.com>
2019-09-25 19:11:48 -0400

    Initial commit

---
+++ UsefulLibrary/Useful.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UsefulLibrary
+{
+    public static class Useful
+    {
+        public static string Greet(string name)
+        {
+            if (name is null)
+                throw new ArgumentNullException(nameof(name));
+            
+            return $"Hello, {name}!";
+        }
+    }
+}
